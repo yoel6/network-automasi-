a0939cd477e0f9438e6da613bdb31164a068d823
yoel6 <58687358+yoel6@users.noreply.github.com>
2021-07-09 22:07:06 +0700

    Update paramiko.py

--- paramiko.py
+++ paramiko.py
@@ -43,6 +43,7 @@ class automation():
             os.system('clear')
             automation.pilihan(self)
     def cekip(self):
+        x = []
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         hostname = ['192.168.122.220','192.168.122.239']
@@ -51,20 +52,29 @@ class automation():
         for h in range(v):
             c = hostname[h]
             print('{}.{}'.format(h+1,c))
-        a = int(input("masukan Pilihan hostname :"))
-        x = hostname[a-1]
-        ssh_client.connect(hostname=x,username="cisco",password="cisco")
-        conn = ssh_client.invoke_shell()
-        conn.send("enable\n")
-        conn.send("cisco\n")
-        conn.send("show ip interface brief\n")
-        conn.send("end\n")
-        time.sleep(1)
-        output = conn.recv(65535)
-        print(output.decode("ascii"))
-        ssh_client.close()
+        a = input("masukan Pilihan hostname :")
+        u = len(a)
+        e = int(u)
+        for q in range(e):
+            t = a[q]
+            x.append(t)
+        print(x)
+        for w in x:
+            y = int(w)
+            x = hostname[y-1]
+            ssh_client.connect(hostname=x,username="cisco",password="cisco")
+            conn = ssh_client.invoke_shell()
+            conn.send("enable\n")
+            conn.send("cisco\n")
+            conn.send("show ip interface brief\n")
+            conn.send("end\n")
+            time.sleep(1)
+            output = conn.recv(65535)
+            print(output.decode("ascii"))
+            ssh_client.close()
         q = input("Apakah Anda Ingin Mengulangnya :")
         if(q == "y"):
+            input
             os.system('clear')
             automation.pilihan(self)
 wiwin = automation()
